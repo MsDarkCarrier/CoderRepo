360100e9e42477a00f95da9a3376e09788ed0193
MsDarkCarrier <skeleton1353@gmail.com>
2022-07-15 15:56:54 -0500

    Segundaprueba

--- Assets/Scripts/Cambios.cs
+++ Assets/Scripts/Cambios.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Nuev : MonoBehaviour
+public class Cambios : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
